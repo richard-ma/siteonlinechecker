9d2f1a0f1e78f247d179098406855c25b14192db
Richard Ma <richard.ma.19850509@gmail.com>
2016-12-28 13:52:31 +0800

    fix tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,8 +1,8 @@
 import pytest
 
-from siteonlinechecker import config
+from siteonlinechecker import loader
 
-def test_load():
-    c = config.Config()
+def test_config_loader():
+    c = loader.Config()
     c.load()
     assert c != None
