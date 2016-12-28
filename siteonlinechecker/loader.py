b9ecfceac8c49c8ca8d0c54b7eb983ff258d3fde
Richard Ma <richard.ma.19850509@gmail.com>
2016-12-28 14:58:03 +0800

    add data class

--- siteonlinechecker/loader.py
+++ siteonlinechecker/loader.py
@@ -5,7 +5,21 @@ class Config(object):
     def __init__(self):
         self.config = None
 
-    def load(self, configFilename = '../config.cfg'):
-        cp = ConfigParser.ConfigParser()
-        self.config = cp.read(configFilename)
+    def load(self, configFilename):
+        self.config = ConfigParser.SafeConfigParser()
+        self.config.read(configFilename)
         return self.config
+
+class Data(list):
+    def __init__(self, dataFilename):
+        super(Data, self).__init__()
+
+        with open(dataFilename) as f:
+            self.extend(f.readlines())
+
+# global config object
+config = Config().load('config.cfg')
+data = Data(config.get('default', 'targets_file'))
+
+if __name__ == '__main__':
+    pass
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,8 +1,25 @@
-import pytest
+import ConfigParser
+from siteonlinechecker.loader import config, data
 
-from siteonlinechecker import loader
+def test_load_config():
+    # load completed?
+    assert isinstance(config, ConfigParser.ConfigParser)
 
-def test_config_loader():
-    c = loader.Config()
-    c.load()
-    assert c != None
+    # default section options
+    assert config.has_section('default')
+    assert config.has_option('default', 'keyword')
+    assert not(config.get('default', 'keyword') is None)
+
+    # smtp section options
+    assert config.has_section('smtp')
+    assert config.has_option('smtp', 'host')
+    assert config.has_option('smtp', 'username')
+    assert config.has_option('smtp', 'password')
+    assert config.has_option('smtp', 'port')
+    assert not(config.get('smtp', 'host') is None)
+    assert not(config.get('smtp', 'username') is None)
+    assert not(config.get('smtp', 'password') is None)
+    assert not(config.get('smtp', 'port') is None)
+
+def test_load_data():
+    assert isinstance(data, list)
