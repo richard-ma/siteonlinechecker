a6d72e61599b8fc2304d58a7247731bdf0b70c38
Richard Ma <richard.ma.19850509@gmail.com>
2017-01-03 15:58:11 +0800

    完善安装脚本

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
-echo "开始安装"
+echo "-------------------------------------------"
+echo "                开始安装                   "
+echo "-------------------------------------------"
 
 echo "正在安装依赖..."
 pip install -r requirements.txt
@@ -11,6 +13,16 @@ cp config.cfg.sample config.cfg
 echo "正在创建检测数据文件targets.data..."
 touch targets.data
 
-echo "安装完成，请先填写config.cfg文件中的相关配置参数。"
-echo "需要监测的网站地址每行一个，写入targets.data文件中，例如："
+echo "-------------------------------------------"
+echo "                开始测试                   "
+echo "-------------------------------------------"
+
+pytest
+
+echo "-------------------------------------------"
+echo "                配置说明                   "
+echo "-------------------------------------------"
+
+echo "1. 请先填写config.cfg文件中的相关配置参数。"
+echo "2. 需要监测的网站地址每行一个，写入targets.data文件中，例如："
 echo "http://www.baidu.com"
