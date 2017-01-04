5f61ce4f8181a6e433a951f163f624665f1a2b34
Richard Ma <richard.ma.19850509@gmail.com>
2017-01-04 16:27:27 +0800

    update

--- siteonlinechecker.sh
+++ siteonlinechecker.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-# start run
-screen -d -m python siteonlinechecker/scheduler.py
+do_start() {
+    screen -d -m python siteonlinechecker/scheduler.py
+}
+
+do_stop() {
+    echo 'stop'
+}
+
+case "$1" in
+  start)
+    do_start
+	;;
+  stop)
+    do_stop
+
+	;;
+  *)
+	echo "Usage: $SCRIPTNAME {start|stop}" >&2
+	exit 3
+
+	;;
+esac
